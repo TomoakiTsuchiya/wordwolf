35ee68ccfd184837f0cea5c8b7fa31f12de80c71
TomoakiTsuchiya <t-tsuchiya@fubic.com>
2019-11-29 01:36:31 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container py-5">
+        <div class="row">
+            <div class="mw-25 mx-auto">
+                <h1>Let's Wordwolf</h1>
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'router'
 }
 </script>
 
 <style>
+body{
+  background-color: #000;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Courgette', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: #fff;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Page1.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>a</div>
+</template>
+
+<script>
+  import store from './Home'
+  export default {
+    name: 'epsode-view',
+    props: { index: Number },
+    methods: {
+    },
+    beforeRouteUpdate (to, from, next) {
+      this.post = null
+      console.log(to)
+    },
+    store: store
+  }
+</script>
